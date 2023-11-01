df5f135521d87cffa990918f18246145572edad4
Joshua Falco Beirer <joshua.falco.beirer@cern.ch>
2023-11-01 15:59:18 +0000

    Add geo definitions

---
+++ pygeosimplify/geo/base.py
@@ -0,0 +1,147 @@
+from __future__ import annotations
+
+from typing import Any
+
+import numpy as np
+
+from pygeosimplify.geo.cells import RPhiZCell, XYZCell
+
+
+class VertexSet:
+    """
+    A class representing a set of vertices.
+
+    Attributes:
+    -----------
+    vertices : numpy.ndarray or None
+        An array of shape (n, 2) representing the (x, y) coordinates of n vertices.
+        If None, the set is empty.
+    """
+
+    def __init__(self, vertices: np.ndarray):
+        self.vertices = vertices
+
+    def __add__(self, other: VertexSet) -> VertexSet:
+        """
+        Returns a new VertexSet that is the union of the two VertexSets.
+
+        Parameters:
+        -----------
+        other : VertexSet
+            The other VertexSet to add.
+
+        Returns:
+        --------
+        VertexSet
+            A new VertexSet that is the union of the two VertexSets.
+        """
+        if self.vertices is None:
+            return other
+        else:
+            return VertexSet(np.vstack((self.vertices, other.vertices)))
+
+
+class Cuboid(VertexSet):
+    """
+    A class representing a cuboid in 3D space. For us a cuboid is simply a collection of vertices attached to a number of attributes.
+    We assume that a cuboid is a convex polyhedra so that we can visualize the cuboid as convex hull of its vertices.
+
+    Attributes:
+    -----------
+    facecolor : str
+        The color of the cuboid's faces.
+    alpha : float
+        The transparency of the cuboid's faces.
+    edgecolor : tuple
+        The color of the cuboid's edges.
+    edgewidth : int
+        The width of the cuboid's edges.
+    """
+
+    def __init__(
+        self,
+        vertices: np.ndarray,
+        facecolor: str = "tab:orange",
+        alpha: float = 0.1,
+        edgecolor: tuple = (1, 1, 1, 1),
+        edgewidth: float = 1,
+    ):
+        super().__init__(vertices)
+
+    def max_extent_in_dim(self, dimIdx: int) -> list:
+        """
+        Returns the minimum and maximum values of the cuboid's vertices in the specified dimension.
+
+        Parameters:
+        -----------
+        dimIdx : int
+            The index of the dimension to compute the extent for.
+
+        Returns:
+        --------
+        list
+            A list containing the minimum and maximum values of the cuboid's vertices in the specified dimension.
+        """
+        values = [vertex[dimIdx] for vertex in self.vertices]
+        return [min(values), max(values)]
+
+    def max_extent(self) -> tuple:
+        """
+        Returns the minimum and maximum values of the cuboid's vertices in all three dimensions.
+
+        Returns:
+        --------
+        tuple
+            A tuple containing three lists, each containing the minimum and maximum values of the cuboid's vertices
+            in one of the three dimensions.
+        """
+        return self.max_extent_in_dim(0), self.max_extent_in_dim(1), self.max_extent_in_dim(2)
+
+
+class RectangularCuboid(Cuboid):
+    """
+    A class representing a rectangular cuboid defined by its dimensions and center position.
+    No implicit coordinate system is assumed at this point.
+
+    Args:
+        di (float): The length of the cuboid along the x-axis.
+        dj (float): The length of the cuboid along the y-axis.
+        dk (float): The length of the cuboid along the z-axis.
+        pos (XYZ, optional): The center position of the cuboid. Defaults to XYZ(0,0,0).
+    """
+
+    def __init__(self, di: float, dj: float, dk: float, pos: Any):
+        self.di = di
+        self.dj = dj
+        self.dk = dk
+        self.pos = pos
+        self.set_vertices()
+        self.has_coordinate_system = False
+
+    def set_vertices(self) -> None:
+        # Calculate half dimensions
+        half_di = self.di / 2
+        half_dj = self.dj / 2
+        half_dk = self.dk / 2
+
+        # Define the eight vertices of the cuboid with the specified center position
+        self.vertices = np.array(
+            [
+                [self.pos.x - half_di, self.pos.y - half_dj, self.pos.z - half_dk],
+                [self.pos.x + half_di, self.pos.y - half_dj, self.pos.z - half_dk],
+                [self.pos.x + half_di, self.pos.y + half_dj, self.pos.z - half_dk],
+                [self.pos.x - half_di, self.pos.y + half_dj, self.pos.z - half_dk],
+                [self.pos.x - half_di, self.pos.y - half_dj, self.pos.z + half_dk],
+                [self.pos.x + half_di, self.pos.y - half_dj, self.pos.z + half_dk],
+                [self.pos.x + half_di, self.pos.y + half_dj, self.pos.z + half_dk],
+                [self.pos.x - half_di, self.pos.y + half_dj, self.pos.z + half_dk],
+            ]
+        )
+
+    def to_RPhiZ(self) -> RPhiZCell:
+        transformed_vertices = np.array([vert.to_RPhiZ() for vert in self.vertices])
+        return RPhiZCell.create_from_vertices(vertices=transformed_vertices)
+
+    def to_XYZ(self) -> XYZCell:
+        transformed_vertices = np.array([vert.to_XYZ() for vert in self.vertices])
+        return XYZCell.create_from_vertices(vertices=transformed_vertices)
---
+++ pygeosimplify/geo/cells.py
@@ -0,0 +1,173 @@
+from __future__ import annotations
+
+import numpy as np
+
+from pygeosimplify.coordinate.definitions import XYZ, EtaPhiR, EtaPhiZ, RPhiZ
+from pygeosimplify.geo.base import RectangularCuboid
+
+
+class RPhiZCell(RectangularCuboid):
+    """
+    A class representing a rectangular cuboid in cylindrical coordinates (r, phi, z).
+    """
+
+    def __init__(self, dr: float, dphi: float, dz: float, pos: RPhiZ = RPhiZ(0, 0, 0)) -> None:  # noqa: B008
+        """
+        Initializes a new instance of the RPhiZCell class.
+
+        Args:
+        - dr (float): The length of the cell in the r direction.
+        - dphi (float): The length of the cell in the phi direction.
+        - dz (float): The length of the cell in the z direction.
+        - pos (RPhiZ): The position of the cell in cylindrical coordinates (r, phi, z).
+
+        Returns:
+        - None
+        """
+        self.dr = dr
+        self.dphi = dphi
+        self.dz = dz
+        self.pos = pos
+        super().__init__(dr, dphi, dz, XYZ(pos.r, pos.phi, pos.z))
+        # Convert all vertices to RPhiZ positions
+        self.vertices = np.array([RPhiZ(vert[0], vert[1], vert[2]) for vert in self.vertices])  # type: ignore[has-type]
+
+    @classmethod
+    def create_from_vertices(cls, vertices: np.ndarray) -> RPhiZCell:
+        """
+        Creates a new instance of the RPhiZCell class from an array of vertices.
+
+        Args:
+        - vertices (np.ndarray): An array of vertices in cylindrical coordinates (r, phi, z).
+
+        Returns:
+        - RPhiZCell: A new instance of the RPhiZCell class.
+        """
+        if not all(isinstance(vert, RPhiZ) for vert in vertices):
+            raise ValueError("Provided vertices for RPhiZ cell must be of type RPhiZ")
+        obj = cls(0, 0, 0, RPhiZ(0, 0, 0))
+        obj.vertices = vertices
+        return obj
+
+
+class XYZCell(RectangularCuboid):
+    """
+    A class representing a rectangular cuboid in 3D space with XYZ coordinates.
+    """
+
+    def __init__(self, dx: float, dy: float, dz: float, pos: XYZ = XYZ(0, 0, 0)) -> None:  # noqa: B008
+        """
+        Initializes a new instance of the XYZCell class.
+
+        Args:
+        - dx (float): The length of the cuboid along the x-axis.
+        - dy (float): The length of the cuboid along the y-axis.
+        - dz (float): The length of the cuboid along the z-axis.
+        - pos (XYZ, optional): The position of the cuboid in 3D space. Defaults to XYZ(0, 0, 0).
+        """
+        self.dx = dx
+        self.dy = dy
+        self.dz = dz
+        self.pos = pos
+        super().__init__(dx, dy, dz, XYZ(pos.x, pos.y, pos.z))
+        # Convert all vertices to XYZ positions
+        self.vertices = np.array([XYZ(vert[0], vert[1], vert[2]) for vert in self.vertices])  # type: ignore[has-type]
+
+    @classmethod
+    def create_from_vertices(cls, vertices: np.ndarray) -> XYZCell:
+        """
+        Creates a new instance of the XYZCell class from an array of vertices.
+
+        Args:
+        - vertices (np.ndarray): An array of vertices representing the corners of the cuboid.
+
+        Returns:
+        - XYZCell: A new instance of the XYZCell class.
+        """
+        if not all(isinstance(vert, XYZ) for vert in vertices):
+            raise ValueError("Provided vertices for XYZ cell must be of type XYZ")
+        obj = cls(0, 0, 0, XYZ(0, 0, 0))
+        obj.vertices = vertices
+        return obj
+
+
+class EtaPhiRCell(RectangularCuboid):
+    """
+    A class representing a rectangular cell in the Eta-Phi-R coordinate system.
+
+    Attributes:
+    - deta (float): The size of the cell in the eta direction.
+    - dphi (float): The size of the cell in the phi direction.
+    - dr (float): The size of the cell in the r direction.
+    - pos (EtaPhiR): The position of the cell in the Eta-Phi-R coordinate system.
+    - vertices (np.ndarray): An array of EtaPhiR objects representing the vertices of the cell.
+
+    Methods:
+    - create_from_vertices(cls, vertices: np.ndarray) -> EtaPhiRCell: A class method that creates an EtaPhiRCell object from an array of vertices.
+    """
+
+    def __init__(self, deta: float, dphi: float, dr: float, pos: EtaPhiR = EtaPhiR(0, 0, 0)):  # noqa: B008
+        self.deta = deta
+        self.dphi = dphi
+        self.dr = dr
+        self.pos = pos
+        super().__init__(deta, dphi, dr, XYZ(pos.eta, pos.phi, pos.r))
+        self.vertices = np.array([EtaPhiR(vert[0], vert[1], vert[2]) for vert in self.vertices])  # type: ignore[has-type]
+
+    @classmethod
+    def create_from_vertices(cls, vertices: np.ndarray) -> EtaPhiRCell:
+        """
+        A class method that creates an EtaPhiRCell object from an array of vertices.
+
+        Args:
+        - vertices (np.ndarray): An array of EtaPhiR objects representing the vertices of the cell.
+
+        Returns:
+        - EtaPhiRCell: An EtaPhiRCell object created from the provided vertices.
+        """
+        if not all(isinstance(vert, EtaPhiR) for vert in vertices):
+            raise ValueError("Provided vertices for EtaPhiR cell must be of type EtaPhiR")
+        obj = cls(0, 0, 0, EtaPhiR(0, 0, 0))
+        obj.vertices = vertices
+        return obj
+
+
+class EtaPhiZCell(RectangularCuboid):
+    """
+    A class representing a rectangular cell in the Eta-Phi-Z coordinate system.
+    """
+
+    def __init__(self, deta: float, dphi: float, dz: float, pos: EtaPhiZ = EtaPhiZ(0, 0, 0)):  # noqa: B008
+        """
+        Initializes an instance of EtaPhiZCell.
+
+        Args:
+        - deta (float): The size of the cell in the eta direction.
+        - dphi (float): The size of the cell in the phi direction.
+        - dz (float): The size of the cell in the z direction.
+        - pos (EtaPhiZ): The position of the cell in the Eta-Phi-Z coordinate system.
+        - vertices (np.ndarray): An array of vertices that define the cell.
+        """
+        self.deta = deta
+        self.dphi = dphi
+        self.dz = dz
+        self.pos = pos
+        super().__init__(deta, dphi, dz, XYZ(pos.eta, pos.phi, pos.z))
+        self.vertices = np.array([EtaPhiZ(vert[0], vert[1], vert[2]) for vert in self.vertices])  # type: ignore[has-type]
+
+    @classmethod
+    def create_from_vertices(cls, vertices: np.ndarray) -> EtaPhiZCell:
+        """
+        Creates an instance of EtaPhiZCell from an array of vertices.
+
+        Args:
+        - vertices (np.ndarray): An array of vertices that define the cell.
+
+        Returns:
+        - An instance of EtaPhiZCell.
+        """
+        if not all(isinstance(vert, EtaPhiZ) for vert in vertices):
+            raise ValueError("Provided vertices for EtaPhiZ cell must be of type EtaPhiZ")
+        obj = cls(0, 0, 0, EtaPhiZ(0, 0, 0))
+        obj.vertices = vertices
+        return obj
