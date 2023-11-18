fe78f207af7c66bb99c4bd0951982455ad89dda9
Joshua Falco Beirer <joshua.falco.beirer@cern.ch>
2023-11-18 18:20:10 +0000

    dummy commit

--- pygeosimplify/utils/message_type.py
+++ pygeosimplify/utils/message_type.py
@@ -5,6 +5,5 @@ class MessageType:
     SUCCESS = "\033[92m[SUCCESS]"
     WARNING = "\033[93m[WARNING]"
     FAIL = "\033[91m[FAILURE] "
-    ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
