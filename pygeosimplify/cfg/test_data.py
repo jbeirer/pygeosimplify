7d89cce4e46f8f3e79fd9726e7156d1beaa48bf2
Joshua Beirer <joshua.beirer@cern.ch>
2024-06-27 11:57:51 +0000

    Add ability to plot cell energies

--- pygeosimplify/cfg/test_data.py
+++ pygeosimplify/cfg/test_data.py
@@ -3,6 +3,10 @@ from pygeosimplify.utils.path_resolver import get_project_root
 DATA_DIR = str(get_project_root()) + "/tests/data/"
 REF_DIR = str(get_project_root()) + "/tests/refs/"
 
+# Data for ATLAS calorimeter cells
 ATLAS_CALO_DATA_DIR = DATA_DIR + "ATLASCaloCells.root"
-
 ATLAS_CALO_DATA_TREE_NAME = "caloDetCells"
+
+# Data of ATLAS calorimeter cells with simulated energy from photon shower
+CELL_ENERGY_DATA_DIR = DATA_DIR + "cell_energy_data.root"
+CELL_ENERGY_DATA_TREE_NAME = "caloDetCells"
--- pygeosimplify/vis/geo.py
+++ pygeosimplify/vis/geo.py
@@ -1,5 +1,7 @@
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
 
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from distinctipy import get_colors
@@ -16,11 +18,14 @@ def plot_geometry(
     df: pd.DataFrame,
     ax: Axes3D = None,
     layer_list: Optional[List[int]] = None,
-    color_list: Optional[Any] = None,
     eta_range: Optional[List] = None,
     phi_range: Optional[List] = None,
     axis_labels: Optional[List] = None,
     unit_scale: float = 1,
+    cell_energy_col: str = None,
+    unit_scale_energy: float = 1,
+    energy_label: str = "Cell Energy",
+    color_map: str = "gist_heat_r",
 ) -> Axes3D:
     """
     Plots the detector cells from pandas data frame
@@ -29,7 +34,6 @@ def plot_geometry(
     df (pd.DataFrame): The pandas DataFrame containing information about the layers, coordinates, and dimensions of cells.
     ax (Axes3D, optional): The 3D axes to plot on. If None, a new figure is created.
     layer_list (List[int], optional): The list of layers to plot. If None, all layers are plotted.
-    color_list (List[Tuple[float, float, float]] | str, optional): The list of colors to use for each layer. If a string is provided, a default color scheme is used.
     eta_range (List, optional): The range of eta values to plot.
     phi_range (List, optional): The range of phi values to plot.
     axis_labels (List, optional): The labels for the x, y, and z axes.
@@ -38,10 +42,6 @@ def plot_geometry(
     Returns:
     Axes3D: The 3D axes containing the plot.
     """
-    if layer_list is None:
-        layer_list = list(df["layer"].unique())
-    if color_list is None:
-        color_list = get_colors(len(layer_list), rng=0)
     if eta_range is None:
         eta_range = [-5, 5]
     if phi_range is None:
@@ -52,14 +52,43 @@ def plot_geometry(
     # Filter for eta and phi range
     df = filter_df_eta_phi(df, eta_range, phi_range)
 
+    # Create a visual cell scene
     vis = CellScene()
 
-    for color_idx, layer_idx in enumerate(layer_list):
-        # Filter for layer
-        layer_cells = df[(df["layer"] == layer_idx)]
-        add_layer_cells_to_scene(layer_cells, vis, color_idx, unit_scale, color_list)
+    if not cell_energy_col:
+        # Create a color dict mapping a layer to a color
+        if layer_list is None:
+            layer_list = list(df["layer"].unique())
+        # Create a color dict mapping a layer to a color
+        layer_color_dict = dict(zip(layer_list, get_colors(len(layer_list), rng=0)))
+        add_cells_to_scene(
+            df=df,
+            scene=vis,
+            unit_scale=unit_scale,
+            unit_scale_energy=unit_scale_energy,
+            layer_color_dict=layer_color_dict,
+        )
+    else:
+        # Create a color map mapping cell energy to a color
+        vmin = df[cell_energy_col].min() * unit_scale_energy
+        vmax = df[cell_energy_col].max() * unit_scale_energy
+        norm = mcolors.LogNorm(vmin * 0.1, vmax)
+
+        add_cells_to_scene(
+            df=df,
+            scene=vis,
+            unit_scale=unit_scale,
+            unit_scale_energy=unit_scale_energy,
+            colormap=plt.get_cmap(color_map),
+            norm=norm,
+        )
 
     ax = vis.plot(ax=ax, axis_labels=axis_labels)
+
+    if cell_energy_col:
+        mappable = plt.cm.ScalarMappable(norm=norm, cmap=plt.get_cmap(color_map))
+        cbar = plt.colorbar(mappable, ax=ax, fraction=0.035, pad=0.15)
+        cbar.set_label(energy_label)
 
     # Regularize x,y limits so that limits are identical for x and y (to avoid distortions)
     minMaxX = vis.min_max_cell_list_extent(0)
@@ -80,12 +109,20 @@ def filter_df_eta_phi(df: pd.DataFrame, eta_range: List, phi_range: List) -> pd.
     return df
 
 
-def add_layer_cells_to_scene(
-    df: pd.DataFrame, scence: CellScene, color_idx: int, unit_scale: float, color_list: List
+def add_cells_to_scene(
+    df: pd.DataFrame,
+    scene: CellScene,
+    unit_scale: float,
+    unit_scale_energy: float,
+    layer_color_dict: Dict = None,
+    colormap: Any = None,
+    norm: mcolors.Normalize = None,
 ) -> None:
     for row in df.itertuples():
         cell = get_cell_from_row(row, unit_scale)
-        scence.add_cell(cell, facecolor=color_list[color_idx], alpha=0.1, edgewidth=0.01)
+        color = layer_color_dict[row.layer] if layer_color_dict else colormap(norm(row.cell_energy * unit_scale_energy))
+
+        scene.add_cell(cell, facecolor=color, alpha=0.1, edgewidth=0.01)
 
 
 def get_cell_from_row(row: pd.DataFrame, unit_scale: float) -> Cell:
